8f18e792bc6f19f6c27c43cc01d1f66958567295
NhiNguyenAI <nhintnguyenAI@gmail.com>
2025-04-22 00:03:07 +0200

    created the draw_circle to test the connection the controller with turtlesim

---
+++ my_robot_controller/my_robot_controller/my_main_node.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+class MyFirstNode(Node):
+    def __init__(self):
+        super().__init__('my_first_node')
+        self.counter = 0
+        # Create a timer that calls the timer_callback every second
+        self.timer = self.create_timer(1.0, self.timer_callback)
+
+    
+    def timer_callback(self):
+        self.get_logger().info('Hello, world! %d' % self.counter)
+        # Increment the counter
+        self.counter += 1
+        
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = MyFirstNode()
+    rclpy.spin(node)
+    
+    rclpy.shutdown()
+if __name__ == '__main__':
+    main()
